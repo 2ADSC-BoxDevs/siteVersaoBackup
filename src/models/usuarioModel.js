3fb263e75366d2b8ecc9155296db67174bef199d
kauan-mendes <kauan.mendes@sptech.school>
2022-11-28 01:08:29 -0300

    Rota de inovação pronta, porem esta com algum possivel problema na rota

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -179,7 +179,8 @@ function alterarMachine(idMachine, sistema, status) {
     console.log("Executando a instrução SQL: \n" + instrucao);
 
     return database.executar(instrucao);
-}
+} 
+
 
 function alterarStatus(isActive, id_maquina) {
     console.log("ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function editar(): ");
